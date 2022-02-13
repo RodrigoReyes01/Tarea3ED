0f8124a325da94d68d3678f544826eed5f10275a
RodrigoReyes01 <71049819+RodrigoReyes01@users.noreply.github.com>
2022-02-12 19:16:13 -0600

    Add files via upload

--- Tarea3ED.py
+++ Tarea3ED.py
@@ -14,6 +14,8 @@ class MyStruct(NamedTuple):
 
 x= int(input("ingrese el punto X: "))
 y= int(input("ingrese el punto Y: "))
+
+#if X
 if (x > 0):
     a = "positive"
 elif (x < 0):
@@ -21,6 +23,7 @@ elif (x < 0):
 else:
     a = ("no es ni negativo ni positivo")
 
+#if Y
 if (y > 0):
     b = "positive"
 elif (y < 0):
@@ -28,6 +31,7 @@ elif (y < 0):
 else:
     b = ("no es ni negativo ni positivo")
 
+#if cuadrante
 if (a == "positive" and b== "positive"):
     c = 1
 elif(a == "negative" and b== "positive"):
@@ -36,6 +40,8 @@ elif(a == "negative" and b== "negative"):
     c = 3
 elif(a == "positive" and b== "negative"):
     c = 4
+elif(a == "no es ni negativo ni positivo" or b == "no es ni negativo ni positivo"):
+    c = 0
 
 
 my_item = MyStruct(x, y, a, b, c)
